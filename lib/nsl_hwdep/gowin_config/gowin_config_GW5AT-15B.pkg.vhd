a2d208a2a70b7ac9fb070f676c374c50c62ed3de
Nicolas Pouillon <nipo@ssji.net>
2025-12-20 10:44:49 +0100

    hwdep/gowin_config: Update config files

--- lib/nsl_hwdep/gowin_config/gowin_config_GW5AT-15B.pkg.vhd
+++ lib/nsl_hwdep/gowin_config/gowin_config_GW5AT-15B.pkg.vhd
@@ -3,55 +3,49 @@ package body gowin_config is
   function iodelay_step_ps return real
   is
   begin
-    return 25.0;
+    return 12.5;
   end function;
 
   function device_name return string
   is
   begin
-    return "GW1N-9C";
+    return "GW5A-15A";
   end function;
 
   function internal_osc return string
   is
   begin
-    return "osc";
+    return "osca";
   end function;
 
   function pll_type return string
   is
   begin
-    return "rpll";
+    return "pll";
   end function;
 
   function pll_vco_fmin return real
   is
   begin
-    return 400.0e6;
+    return 650.0e6;
   end function;
 
   function pll_vco_fmax return real
   is
   begin
-    return 1200.0e6;
+    return 1300.0e6;
   end function;
 
   function pll_pfd_fmin return real
   is
   begin
-    return 3.0e6;
+    return 19.0e6;
   end function;
 
   function pll_pfd_fmax return real
   is
   begin
-    return 400.0e6;
-  end function;
-
-  function pll_odiv_possibilities return ivec
-  is
-  begin
-    return (2,4,8,16,32,48,64,80,96,112,128);
+    return 81.25e6;
   end function;
 
 end package body;
